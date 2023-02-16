a8f9be8c7387e4a5765062e6e5e118e9caa2dc67
Gi Almeida <giovanna.almeida.sousa1@gmail.com>
2023-02-16 10:58:12 -0300

    Atividades do módulo 3

---
+++ 01-OlaMundo/src/br/com/fiap/banco/Conta.java
@@ -0,0 +1,75 @@
+package br.com.fiap.banco;
+
+/**
+ * Classe que abstrai uma Conta bancária
+ * @author AlmeidaGih
+ * @version 1.0
+ */
+public class Conta {
+	
+	/**
+	 * Número da conta
+	 */
+	private int numero;
+	/**
+	 * Número da agência
+	 */
+	private int agencia;
+	/**
+	 * Saldo da conta
+	 */
+	private double saldo;
+	
+	public Conta() {
+		
+	}
+	
+	public Conta(int numero, int agencia, double saldo) {
+		this.setNumero(numero);
+		this.setAgencia(agencia);
+		this.setSaldo(saldo);
+	}
+	
+	/**
+	 * Deposita um valor ao saldo da conta
+	 * @param valor Valor a ser depositado
+	 */
+	public void depositar(double valor) {
+		this.saldo += valor;
+	}
+	
+	/**
+	 * Retira um valor do saldo da conta
+	 * @param valor Valor a ser retirado
+	 */
+	public void retirar(double valor) {
+		this.saldo -= valor;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public int getAgencia() {
+		return agencia;
+	}
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	/**
+	 * Verifica o saldo da conta 
+	 * @return Valor do saldo da conta
+	 */
+	public double verificarSaldo() {
+		return saldo;
+	}
+}
---
+++ 01-OlaMundo/src/br/com/fiap/banco/teste/Teste.java
@@ -0,0 +1,32 @@
+package br.com.fiap.banco.teste;
+
+import br.com.fiap.banco.Conta;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Conta cc = new Conta();
+		cc.setSaldo(50);
+		cc.setAgencia(123);
+		cc.setNumero(321);
+		
+		cc.depositar(100);
+		
+		System.out.println(cc.verificarSaldo());
+		
+		Conta poupanca = new Conta(111,222,1000);
+		
+		poupanca.retirar(50);
+		
+		System.out.println(poupanca.verificarSaldo());
+		
+		Conta cc2 = null;
+		if(cc2 != null) {
+			System.out.println("Existe uma conta");
+		}else {
+			System.out.println("Não existe uma conta");
+		}
+	}
+
+}
